9211cc3134ab4a7e85094bf6ffb0798888f36a72
yashshree <yashshree29.chavan@gmail.com>
2019-05-02 12:31:58 +0530

    sharedpreference class done

--- app/src/main/java/com/app/projecttemplete/application/Application.kt
+++ app/src/main/java/com/app/projecttemplete/application/Application.kt
@@ -1,6 +1,11 @@
 package com.app.projecttemplete.application
 
 import android.app.Application
+import com.app.projecttemplete.utils.SharedPreferenceUtil
 
 class Application: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        SharedPreferenceUtil.initialize(applicationContext)
+    }
 }
--- app/src/main/java/com/app/projecttemplete/view/activities/MainActivity.kt
+++ app/src/main/java/com/app/projecttemplete/view/activities/MainActivity.kt
@@ -6,10 +6,9 @@ import android.os.Bundle
 import com.app.projecttemplete.R
 import com.app.projecttemplete.listeners.OnAlertDialogRespondListener
 import com.app.projecttemplete.listeners.OnSnackbarButtonClicked
-import com.app.projecttemplete.utils.showAlertDialog
-import com.app.projecttemplete.utils.showSnackBar
-import com.app.projecttemplete.utils.showToast
-import com.app.projecttemplete.utils.showView
+import com.app.projecttemplete.models.LoginResponse
+import com.app.projecttemplete.utils.*
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.layout_progressbar.*
 
@@ -18,6 +17,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
+        
+
+/*
+       val data= LoginResponse(1,"yashshree")
+
+        val loginJson=GsonBuilder().create().toJson(data)
+
+        SharedPreferenceUtil.saveLoginData(loginJson)
+
+        txtHelloWorld.text=SharedPreferenceUtil.getLoginData()?.id.toString() +SharedPreferenceUtil.getLoginData()?.name.toString()
+
+*/
 
        /* showAlertDialog("error","network connection",listener = object : OnAlertDialogRespondListener{
             override fun onPositiveButtonClicked() {
